19a59522d95313d9b9c6754c8af79e7d9f2463ba
Hamzeh Kaddoura <kadd0024@algonquinlive.com>
2023-08-18 16:29:23 -0400

    a clean initial structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Hamzeh, you're on your way to becoming a software developer!
         </p>
         <a
           className="App-link"
